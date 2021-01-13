9f8fe469bd156c56536ad00993fe87035ee5f026
purnendu samanta <joysamanta123@gmail.com>
2021-01-13 16:10:29 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,66 +12,73 @@ class MyApp extends StatelessWidget {
       home:Scaffold(
         backgroundColor:Colors.teal,
         body:SafeArea(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:<Widget> [
-              CircleAvatar(
-              backgroundImage:AssetImage('images/joy.jpg'),
-                radius: 50,
-              ),
-              Text("Purnendu Samanta",
-                style: TextStyle(
-                  fontFamily:'NerkoOne',
-                  fontSize: 40,
-                  color: Colors.white,
-                ),
-              ),
-              Text("FLUTTER DEVELOPER",
-                style: TextStyle(
-                  fontFamily:'SourceSansPro',
-                  fontSize: 20,
-                  color: Colors.white70,
-                  letterSpacing: 2.5,
-                  //fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20,width: 150,
-                child:Divider(
-                  color: Colors.teal[100],
-                ),
-              ),
-              Card(
-                color:Colors.white,
-                margin:EdgeInsets.symmetric(vertical: 10,horizontal: 50),
-                child: ListTile(
-                  leading:Icon(Icons.phone,
-                    color: Colors.teal),
-                  title: Text('+91 9614472290',
-                      style: TextStyle(
-                        color:Colors.teal,
-                        fontFamily: 'SourceSansPro',
-                        fontSize: 20,
-                      ),
-                ),
-              ),
-              ),
-              Card(
-                color:Colors.white,
-                margin:EdgeInsets.symmetric(vertical: 10,horizontal: 50),
-                child: ListTile(
-                  leading: Icon(Icons.email,
-                    color: Colors.teal),
-                  title:Text('joysamanta123@gmail.com',
+          child:LayoutBuilder(
+            builder:(BuildContext context,BoxConstraints constraints)
+            {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:<Widget> [
+                  CircleAvatar(
+                    backgroundImage:AssetImage('images/joy.jpg'),
+                    radius: constraints.maxHeight*.08,
+                  ),
+                  Text("Purnendu Samanta",
+                    textAlign: TextAlign.center,
                     style: TextStyle(
-                      color:Colors.teal,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 13,
+                      fontFamily:'NerkoOne',
+                      fontSize: constraints.maxHeight*.06,
+                      color: Colors.white,
                     ),
                   ),
-                ),
-              ),
-            ],
+                  Text("FLUTTER DEVELOPER",
+                    style: TextStyle(
+                      fontFamily:'SourceSansPro',
+                      fontSize: constraints.maxHeight*.03,
+                      color: Colors.white70,
+                      letterSpacing: 2.5,
+                      //fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height:constraints.maxHeight*.01,width: constraints.maxHeight*.04,
+                    child:Divider(
+                      color: Colors.teal[100],
+                    ),
+                  ),
+                  Card(
+                    color:Colors.white,
+                    margin:EdgeInsets.symmetric(vertical: constraints.maxHeight*.01,horizontal: constraints.maxHeight*.04),
+                    child: ListTile(
+                      leading:Icon(Icons.phone,
+                          color: Colors.teal),
+                      title: Text('+91 9614472290',
+                        style: TextStyle(
+                          color:Colors.teal,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: constraints.maxHeight*.03,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color:Colors.white,
+                    margin:EdgeInsets.symmetric(vertical: constraints.maxHeight*.01,horizontal: constraints.maxHeight*.04),
+                    child: ListTile(
+                      leading: Icon(Icons.email,
+                          color: Colors.teal),
+                      title:Text('joysamanta123@gmail.com',
+                        style: TextStyle(
+                          color:Colors.teal,
+                          fontFamily: 'SourceSansPro',
+                          fontSize:constraints.maxHeight*.025,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+
           ),
         ),
       ),
